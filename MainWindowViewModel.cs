61e66c7d6ed150ffcd923c35c2dfba3311fb1e87
Kevin Sieg <Kevin.Sieg@ms-ultrasonic.de>
2021-11-22 18:15:27 +0100

    Projektdateien hinzufügen.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MweMahAppsRangeSliderBug
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private Visibility __exampleSliderVisible = Visibility.Visible;
+        public Visibility ExampleSliderVisible
+        {
+            get { return __exampleSliderVisible; }
+            set { __exampleSliderVisible = value; }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.ExampleChangeVisibility = new SimpleCommand(
+                o => true,    
+                x => ToggleVisibility()
+                );
+        }
+
+        public ICommand ExampleChangeVisibility { get; }
+
+        public void ToggleVisibility()
+        {
+            if(ExampleSliderVisible == Visibility.Visible)
+            {
+                ExampleSliderVisible = Visibility.Collapsed;
+            }
+            else
+            {
+                ExampleSliderVisible = Visibility.Visible;
+            }
+
+            this.OnPropertyChanged(nameof(this.ExampleSliderVisible));
+        }
+    }
+}
